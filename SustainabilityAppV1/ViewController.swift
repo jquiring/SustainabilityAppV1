93e5e47069157399393df2d961b9b8f26eed4e22
jquiring <jake.p.quiring@gmail.com>
2014-10-20 13:25:03 -0700

    Jakes change

--- SustainabilityAppV1/ViewController.swift
+++ SustainabilityAppV1/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loginButtonLabel.text = "First Selected V2"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
