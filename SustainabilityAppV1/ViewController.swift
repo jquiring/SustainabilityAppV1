da055de9514d19a28c64848e5225683987c2ceb1
hsakuli <ilukash@mac.com>
2014-10-31 15:01:50 -0700

    LDAP Authentication and Login Button Login implemented. LDAP Not working ( unsure whether it is the connection between computers or Xcode causing problems)

--- SustainabilityAppV1/ViewController.swift
+++ SustainabilityAppV1/ViewController.swift
@@ -7,17 +7,70 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController,UITextFieldDelegate {
 
-  
+    //LDAP Variables
+    let LDAPIP = "147.222.165.133:8000"
+    
+    
+    
     @IBOutlet weak var email: UITextField!
-
     @IBOutlet weak var password: UITextField!
-   
+    @IBOutlet weak var incorrectLoginLabel: UILabel!
+    
     @IBAction func login(sender: AnyObject) {
         //if ldap confirmed && user in DB login 
         //if ladap confirmed -- new user
+        //set the incorrectLoginLabel.hidden = false if the login has failed
+        
+        var VC1 = self.storyboard?.instantiateViewControllerWithIdentifier("newUser") as NewUserController
+        let navController = UINavigationController(rootViewController: VC1)
+        // Creating a navigation controller with VC1 at the root of the navigation stack.
+        self.presentViewController(navController, animated:true, completion: nil)
+        
+        var request = NSMutableURLRequest(URL: NSURL(string: "147.222.165.133:2000")!)
+        var session = NSURLSession.sharedSession()
+        request.HTTPMethod = "POST"
+        
+        var params = ["username":email.text, "password":password.text] as Dictionary<String, String>
+        
+        var err: NSError?
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            println("Inside completion handler")
+            println("Response: \(response)")
+            var strData = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println("Body: \(strData)")
+            var err: NSError?
+            var json = NSJSONSerialization.JSONObjectWithData(data, options: .MutableLeaves, error: &err) as? NSDictionary
+            
+            // Did the JSONObjectWithData constructor return an error? If so, log the error to the console
+            if(err != nil) {
+                println(err!.localizedDescription)
+                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                println("1 Error could not parse JSON: '\(jsonStr)'")
+            }
+            else {
+                // The JSONObjectWithData constructor didn't return an error. But, we should still
+                // check and make sure that json has a value using optional binding.
+                if let parseJSON = json {
+                    // Okay, the parsedJSON is here, let's get the value for 'success' out of it
+                    println(parseJSON)
+                }
+                else {
+                    // Woa, okay the json object was nil, something went worng. Maybe the server isn't running?
+                    let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                    println("2 Error could not parse JSON: \(jsonStr)")
+                }
+            }
+        })
+        task.resume()
+        sleep(5)
     }
 
 
@@ -26,8 +79,8 @@ class ViewController: UIViewController,UITextFieldDelegate {
         self.navigationController?.setNavigationBarHidden(true, animated: true)
         email!.delegate = self
         password!.delegate = self
+        incorrectLoginLabel.hidden = true
 
-    
         // Do any additional setup after loading the view, typically from a nib.
     }
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
