1230702149017af1c0e0172ece04b38fbde82b47
hsakuli <ilukash@mac.com>
2014-11-21 15:15:53 -0800

    Edit User should work (Needs testing)

--- SustainabilityAppV1/EditUserController.swift
+++ SustainabilityAppV1/EditUserController.swift
@@ -7,29 +7,104 @@
 //
 
 import UIKit
+import Foundation
 
 class EditUserController: UIViewController {
-    
-    @IBOutlet weak var first_name: UITextField!
-    @IBOutlet weak var last_name: UITextField!
+    var flag = false
+    @IBOutlet weak var first_name_field: UITextField!
+    @IBOutlet weak var last_name_field: UITextField!
     @IBOutlet weak var phone_number: UITextField!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var error_label: UILabel!
     
-    @IBAction func save(sender: AnyObject) {
+    @IBAction func save(sender: AnyObject){
+        var flag_Val = false
+        var return_Val = -1
+        var request = NSMutableURLRequest(URL: NSURL(string: "http://147.222.165.121:8000/edituser/")!)
+     
+        
+        
+        var session = NSURLSession.sharedSession()
+        
+        //parameter values cahnge these
+        var username = "tmiller12"     //this will be NSUserDefaults.standardUserDefaults().objectForKey("username") as String
+
+        var first_name = first_name_field.text
+        var last_name = last_name_field.text
+        var p_email = email.text            //editable field
+        var phone = phone_number.text       //editable field
+        
+        var params = ["username":username, "first_name":first_name, "last_name":last_name, "pref_email":p_email, "phone":phone] as Dictionary<String, String>
+        
+        //Load body with JSON serialized parameters, set headers for JSON! (Star trek?)
+        var err: NSError?
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &err)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+        var task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            
+            var message = ""
+            var json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(0), error: &err) as? NSDictionary
+            if(err != nil) {
+                println(err!.localizedDescription)
+                let jsonStr = NSString(data: data, encoding: NSUTF8StringEncoding)
+                println("Error could not parse JSON: '\(jsonStr)'")
+            }
+            else{
+                if let parseJSON = json as? Dictionary<String,AnyObject>{
+                    message = parseJSON["message"] as String
+                }
+            }
+            //downcast NSURLResponse object to NSHTTPURLResponse
+            if let httpResponse = response as? NSHTTPURLResponse {
+                //get the status code
+                var status_code = httpResponse.statusCode
+                
+                //200 = OK: user created, carry on!
+                if(status_code == 200){
+                    println(message)
+                    return_Val = 200
+                    flag_Val = true
+                }
+                    //400 = BAD_REQUEST: error in creating user, display error!
+                else if(status_code == 400){
+                    println(message)
+                    return_Val = 400
+                    flag_Val = true
+                }
+                    //500 = INTERNAL_SERVER_ERROR. Oh snap *_*
+                else if(status_code == 500){
+                    println(message)
+                    return_Val = 500
+                    flag_Val = true
+                }
+            } else {
+                println("Error in casting response, data incomplete")
+            }
+        })
+        task.resume()
+        while(flag == false){
+            flag = flag_Val
+        }
+        if(return_Val == 200){
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
     }
     @IBAction func cancel(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        /*
-        first_name.text = NSUserDefaults.standardUserDefaults().objectForKey("first_name") as String
-        last_name.text = NSUserDefaults.standardUserDefaults().objectForKey("last_name") as String
+        
+        first_name_field.text = NSUserDefaults.standardUserDefaults().objectForKey("first_name") as String
+        last_name_field.text = NSUserDefaults.standardUserDefaults().objectForKey("last_name") as String
         phone_number.text = NSUserDefaults.standardUserDefaults().objectForKey("phone") as String
         email.text = NSUserDefaults.standardUserDefaults().objectForKey("pref_email") as String
+
         error_label.hidden = true
-*/
+
 
     }
 
@@ -39,7 +114,7 @@ class EditUserController: UIViewController {
     }
     
     func checkFields() -> Bool {
-        if(first_name.text.isEmpty || last_name.text.isEmpty) {
+        if(first_name_field.text.isEmpty || last_name_field.text.isEmpty) {
             //var alert = UIAlertController(title: "Warning", message: "Please include a last and first name", preferredStyle: UIAlertControllerStyle.Alert)
             //self.presentViewController(alert, animated: true, completion: nil)
             //alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { (action: UIAlertAction!) in }))
