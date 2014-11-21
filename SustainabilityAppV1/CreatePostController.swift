3bf5c04b4a14ab2b66e45acef8568e3c23e6400b
jquiring <jake.p.quiring@gmail.com>
2014-11-21 12:34:57 -0800

    Pickers Pickers Pickers
    working with pickers

--- SustainabilityAppV1/CreatePostController.swift
+++ SustainabilityAppV1/CreatePostController.swift
@@ -26,38 +26,79 @@ class CreatePostController: UITableViewController, UIPickerViewDataSource, UIPic
     @IBOutlet var image1: UIImageView!
     @IBOutlet var image2: UIImageView!
     @IBOutlet var image3: UIImageView!
-  
+    let date_picker:UIDatePicker = UIDatePicker()
    // @IBOutlet var rideDetials: UITableViewSection!
     @IBOutlet var descOutlet: UITextView!
     @IBOutlet var title_field: UITextField!
-    @IBOutlet var cat_picker: UIPickerView!
+    weak var cat_picker: UIPickerView!
     @IBAction func cancel(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
    
     let pickerData = ["Books","Electronics","Furniture","Appliances & Kitchen","Ride Shares","Services","Events","Recreation","Clothing"]
-
-    let categoryTitles = ["Category","Title","Description","Pictures","Price","Ride Details","How would you like to be contacted?"]
+    let categoryTitles = ["  Category","  Title","  Description","  Pictures","  Price","  Ride Details","  How would you like to be contacted?"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        cat_picker.delegate = self
-        cat_picker.dataSource = self
+
         picker!.delegate=self
+        //intializeCatPicker()
+        initializeDatePicker()
         navigationController?.navigationBar.barStyle = UIBarStyle.Default
         navigationController?.navigationBar.barTintColor = UIColor(red: 0.633, green: 0.855, blue: 0.620, alpha: 1)
         navigationController?.navigationBar.titleTextAttributes = [NSFontAttributeName: UIFont(name: "HelveticaNeue-Light",size: 24)!,NSForegroundColorAttributeName: UIColor.darkGrayColor()]
+        
         setUpImageGestures()
 
         //image1.
         // Do any additional setup after loading the view.
     }
+    func doneAction() {
+ 
+        self.title_field.resignFirstResponder()
+    }
+    //TODO: title field changed to cat_field.
+    func intializeCatPicker(){
+        var item = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Done,
+            target: self, action: "doneAction")
+        var toolbar = UIToolbar(frame: CGRectMake(0, 0, self.view.bounds.size.width, 44))
+        toolbar.setItems([item], animated: true)
+        self.title_field.inputAccessoryView = toolbar
+        var tempPicker = UIPickerView()
+        tempPicker.delegate = self
+        tempPicker.dataSource = self
+        self.cat_picker = tempPicker
+        self.title_field.inputView = cat_picker
+    }
+    func initializeDatePicker(){
+        var item = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Done,
+            target: self, action: "doneAction")
+        var toolbar = UIToolbar(frame: CGRectMake(0, 0, self.view.bounds.size.width, 44))
+        toolbar.setItems([item], animated: true)
+        self.title_field.inputAccessoryView = toolbar
+        
+        let currentDate = NSDate()  //5 -  get the current date
+        date_picker.minimumDate = currentDate
+        self.title_field.inputView = date_picker
+        date_picker.addTarget(self, action: Selector("dataPickerChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+    }
+    func dataPickerChanged(date_picker:UIDatePicker) {
+        var dateFormatter = NSDateFormatter()
+        
+        dateFormatter.dateStyle = NSDateFormatterStyle.ShortStyle
+        dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        
+        var strDate = dateFormatter.stringFromDate(date_picker.date)
+        title_field.text = strDate
+    }
     func setUpImageGestures(){
         
         let gestureRecogniser1 = UITapGestureRecognizer(target: self, action: Selector("image1Toutched"))
         self.image1.addGestureRecognizer(gestureRecogniser1)
-        let scroll = UITapGestureRecognizer(target: self, action: Selector("tableToutched"))
-        self.view.addGestureRecognizer(scroll)
+        let tap = UITapGestureRecognizer(target: self, action: Selector("tableToutched"))
+        //let scroll = UIPanGestureRecognizer(target: self, action: Selector("tableToutched"))
+        //self.view.addGestureRecognizer(scroll)
+        self.view.addGestureRecognizer(tap)
         let gestureRecogniser2 = UITapGestureRecognizer(target: self, action: Selector("image2Toutched"))
         self.image2.addGestureRecognizer(gestureRecogniser2)
         
@@ -113,6 +154,7 @@ class CreatePostController: UITableViewController, UIPickerViewDataSource, UIPic
     }
     func tableToutched(){
         title_field.resignFirstResponder()
+        descOutlet.resignFirstResponder()
         println("table touched")
     }
     //TODO: change these to gray/colored images of contact options
@@ -132,6 +174,10 @@ class CreatePostController: UITableViewController, UIPickerViewDataSource, UIPic
         else{
             self.pEmail.image = UIImage(named:"bike.jpg")
         }
+    }
+    func pickerView(pickerView: UIPickerView!, didSelectRow row: Int, inComponent component: Int)
+    {
+        title_field.text = "\(pickerData[row])"
     }
     func textToutched(){
         var oldImage:UIImage = self.text.image!
