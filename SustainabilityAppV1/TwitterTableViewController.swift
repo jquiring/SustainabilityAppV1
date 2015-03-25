6b0ce67ad374506f9e8cef4d983611dae9d1baca
hsakuli <ilukash@mac.com>
2015-03-25 16:54:24 -0700

    Twitter Kinda working and TaA take 2

--- SustainabilityAppV1/TwitterTableViewController.swift
+++ SustainabilityAppV1/TwitterTableViewController.swift
@@ -18,7 +18,7 @@ class TwitterTableViewController: UITableViewController , TWTRTweetViewDelegate 
             tweetTable.reloadData()
         }
     }
-    let tweetIDs = ["20", // @jack's first Tweet
+    let tweetIDs = ["2011", // @jack's first Tweet
         "510908133917487104"] // our favorite bike Tweet
     
     @IBOutlet var tweetTable: UITableView!
@@ -30,11 +30,21 @@ class TwitterTableViewController: UITableViewController , TWTRTweetViewDelegate 
         tweetTable.registerClass(TWTRTweetTableViewCell.self, forCellReuseIdentifier: tweetTableReuseIdentifier)
         
         // Load Tweets
-        Twitter.sharedInstance().APIClient.loadTweetsWithIDs(tweetIDs) { tweets, error in
-            if let ts = tweets as? [TWTRTweet] {
-                self.tweets = ts
+        Twitter.sharedInstance().logInGuestWithCompletion { guestSession, error in
+            if (guestSession != nil) {
+                Twitter.sharedInstance().APIClient.loadUserWithID("1601601674") { user, error in
+                    if let userID = user as TWTRUser?{
+                        println(user.description)
+                        //self.tweetIDs = usersWithJSONArray(userID)
+                        Twitter.sharedInstance().APIClient.loadTweetsWithIDs(self.tweetIDs) { tweets, error in
+                            if let ts = tweets as? [TWTRTweet] {
+                                self.tweets = ts
+                            } else {
+                                println("Failed to load tweets: \(error.localizedDescription)")
+                            }
+                        }}}
             } else {
-                println("Failed to load tweets: \(error.localizedDescription)")
+                println("error: \(error.localizedDescription)");
             }
         }
     }
@@ -48,18 +58,19 @@ class TwitterTableViewController: UITableViewController , TWTRTweetViewDelegate 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return tweets.count
     }
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let tweet = tweets[indexPath.row]
         let cell = tableView.dequeueReusableCellWithIdentifier(tweetTableReuseIdentifier, forIndexPath: indexPath) as TWTRTweetTableViewCell
         cell.tweetView.delegate = self
+        cell.configureWithTweet(tweet)
         return cell
     }
     
